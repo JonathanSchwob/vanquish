4be5e3f7fc98e4e2e4c1baeb8141efd59d9dea10
Jonathan Schwob <jrschwob@gmail.com>
2023-02-10 17:23:32 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from "react";
+import Player from "./components/Player";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Player />
+    </div>
+  );
+}
+
+export default App;
+
+// player
+// enemy
+// hand
+//   -cards
+// deck
+// graveyard
